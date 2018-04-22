5b486a6133f0792ec6691ec84991d57fb1232706
vineet7 <cvineet49@gmail.com>
2018-04-22 23:42:38 +0530

    prsnl

--- play.py
+++ play.py
@@ -13,8 +13,8 @@ CLOUD_VISION_ENDPOINT_URL = 'https://vision.googleapis.com/v1/images:annotate'
 from googleapiclient.discovery import build
 import pprint
 
-api_key = "YOUR_GOOGLE_API_KEY"
-cse_id = "YOUR_CUSTOM_GOOGLE_SEARCH_ENGINE_ID"
+api_key = "AIzaSyBfq75QTYNtS-lnq0NyP4HRG-ZzCwcv11k"
+cse_id = "017069261294340804586:alpuolwko9e"
 
 def google_search(search_term, api_key, cse_id, **kwargs):
     service = build("customsearch", "v1", developerKey=api_key)
